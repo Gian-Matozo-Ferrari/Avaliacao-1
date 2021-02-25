a9eed3852ba717e02bbc1c88aa07308e4f2e476b
Gian-Matozo-Ferrari <gianmatozoferrari@outlook.com.br>
2021-02-24 23:04:23 -0300

    Revert "Revert "Adição das imagens""
    This reverts commit 469f3a3cc73923fec0d247e0a3c0a28603b62f32.

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -92,7 +92,7 @@
               <div class="carousel-caption">
                 <h1>Azimut S Collection</h1>
                 <p>Esportividade e tecnologia são os dois concepts de projeto ao redor dos quais nascerem os iates da Coleção S.</p>
-                <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+                <p><a class="btn btn-lg btn-primary" href="#">Fazer Orçamento</a></p>
               </div>
             </div>
           </div>
@@ -103,7 +103,7 @@
               <div class="carousel-caption text-end">
                 <h1>Flybridge Collection.</h1>
                 <p>A Ciência do Luxo</p>
-                <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+                <p><a class="btn btn-lg btn-primary" href="#">Fazer Orçamento</a></p>
               </div>
             </div>
           </div>
@@ -140,23 +140,38 @@
           <div class="col-lg-4">
             <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
     
-            <h2>Heading</h2>
-            <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
-            <p><a class="btn btn-secondary" href="#">View details »</a></p>
+            <h2>S Collection</h2>
+            <p>Esportividade e tecnologia são os dois concepts de projeto ao redor dos quais nascerem os iates da Coleção S.
+              Modelos que incentivam e favorecem o prazer da vida ao ar livre através de hard top de abrir e soluções hi-tech
+              pensadas para conjugar a exigência de funcionalidade com a atenção ao aspecto estético. Uma mistura equilibrada
+              de conforto, prazer de condução e luxo inesperados dentro do mundo dos iates esportivos. O mar, nos rastros da
+              emoção do estilo Made in Italy.
+            </p>
+            <p><a class="btn btn-secondary" href="#">Ver Detalhes</a></p>
           </div><!-- /.col-lg-4 -->
           <div class="col-lg-4">
             <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
     
-            <h2>Heading</h2>
-            <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
-            <p><a class="btn btn-secondary" href="#">View details »</a></p>
+            <h2>Grande Collection</h2>
+            <p>Grande em termos de espaço generoso, no conforto, no nível de serviço e na abundância de elegância. 
+              A coleção Azimut Grande tem tudo o que um proprietário poderia pedir de um carro-chefe e oferece um equilíbrio perfeito 
+              entre o fascínio do estilo italiano e os conceitos de design voltados para a criação de excelência.
+
+
+            </p>
+            <p><a class="btn btn-secondary" href="#">Ver Detalhes</a></p>
           </div><!-- /.col-lg-4 -->
           <div class="col-lg-4">
             <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
     
-            <h2>Heading</h2>
-            <p>And lastly this, the third column of representative placeholder content.</p>
-            <p><a class="btn btn-secondary" href="#">View details »</a></p>
+            <h2>FlyBridge Collection</h2>
+            <p>Agilidade, potência, elegância, escolha. A Coleção Frybridge de Azimut oferece a mais ampla gama no mundo, com modelos que vão dos 42 aos 100 pés,
+               cada um com um conceito de realização caracterizador, cada um com uma versatilidade que permite ao Armador a personalização dos ambientes graças à escolha 
+               dos tecidos, dos couros, das essências de madeira. 
+               Modelos diferentes mas com um denominador comum: a requintada elegância do Design Italiano. Invenções habitacionais que tornam cada cabine uma suíte, materiais 
+               prestigiosos que evidenciam o charme dos interiores, amplas janelas que criam um contato contínuo com o ambiente externo. Coleção Flybridge, o mar, com estilo.
+            </p>
+            <p><a class="btn btn-secondary" href="#">Ver Detalhes</a></p>
           </div><!-- /.col-lg-4 -->
         </div><!-- /.row -->
     
@@ -171,7 +186,7 @@
             <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
           </div>
           <div class="col-md-5">
-            <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+            <img src="{{ asset('images/popa77S.jpg') }}" width="500" height="500"/>
     
           </div>
         </div>
@@ -184,7 +199,7 @@
             <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
           </div>
           <div class="col-md-5 order-md-1">
-            <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+            <img src="{{ asset('images/proa77S.jpg') }}" width="400" height="400" />
     
           </div>
         </div>
@@ -197,7 +212,7 @@
             <p class="lead">And yes, this is the last block of representative placeholder content. Again, not really intended to be actually read, simply here to give you a better view of what this would look like with some actual content. Your content.</p>
           </div>
           <div class="col-md-5">
-            <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+            <img src="{{ asset('images/traseira77S.jpg') }}" width="500" height="500" />
     
           </div>
 >>>>>>> parent of a3a4a67 (Finalização do site)
